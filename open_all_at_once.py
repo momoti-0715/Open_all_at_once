7baff1f2dafaf3d3ed0f8e19954908b25ccaad60
momoti-0715 <ayapon2003@outlook.jp>
2026-04-19 02:08:04 +0900

    json形式で実行できるように変更

--- open_all_at_once.py
+++ open_all_at_once.py
@@ -1,28 +1,65 @@
 import subprocess
 import webbrowser
+import json
+import os
 
-def web_open():
+def load_json(file_path):
+    if not os.path.exists(file_path):
+        print(f"ファイルが存在しません: {file_path}")
+        return None
+
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+        return data
+    except json.JSONDecodeError as e:
+        print(f"JSONの読み込みに失敗しました: {e}")
+        return None
+
+
+def web_open(u_data):
     """urlをまとめて開く"""
-    url_list = []
+    if(u_data["is_open"] == "False"):
+        return
+    
+    b_path = u_data["browser"]
+    if not b_path is None:
+        print(b_path)
+        browser = webbrowser.get(f'"{b_path}" %s')
 
+    
+    url_list = u_data["url_list"]
     for url in url_list:
-        webbrowser.open(url)
+        browser.open(url)
 
     return 0
 
-def cmd_open():
-    subprocess.Popen('start /d "C:\\Users\\User\\Desktop" conda activate py39', shell=True) #/d ファイルのパス　でカレントディレクトリを指定
+def cmd_open(c_data):
+    if(c_data["is_open"] == "False"):
+        return
+
+    p_c_list = c_data["p_c_list"]
+    for p_c in p_c_list:
+        path = p_c.get("path")
+        command = p_c.get("command")
+        if not path is None:
+            if not command is None:
+                subprocess.Popen(['start','/d', path, command], shell=True)
+            subprocess.Popen(['start','/d', path], shell=True)
+        elif not command is None:
+            subprocess.Popen(['start', command], shell=True)
+        else:
+            subprocess.Popen(['start'], shell=True)
 
 
-def task_open():
+
+def task_open(t_data):
     """タスクをまとめて開く"""
-    vscode = "C:\\Users\\User\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
-    task_scheduler = "C:\\windows\\system32\\taskschd.msc"
-    task_manager = "C:\\windows\\system32\\Taskmgr.exe"
+    if(t_data["is_open"] == "False"):
+        return
 
-    tasklist = [vscode, task_scheduler, task_manager]
-
-    for task in tasklist:
+    task_list = t_data["task_list"]
+    for task in task_list:
         try:
             subprocess.Popen(task, shell=True) # taskをまとめて開く
         except:
@@ -31,6 +68,10 @@ def task_open():
     return 0
 
 if __name__ == '__main__':
-    task_open()
-    cmd_open()
-    web_open()
+    file_path = "setting.json"  # JSONファイル名
+    data = load_json(file_path)
+
+    if data:
+        task_open(data["task_open"])
+        cmd_open(data["cmd_open"])
+        web_open(data["web_open"])
